1b7f4a5c1195e44609727928503012eee627e968
Nui Rattapon <nui.rattapon.work@gmail.com>
2022-01-30 17:46:23 +1200

    Init

---
+++ content.js
@@ -0,0 +1,11 @@
+console.log("YY N G run");
+var gt = document.querySelector(".ytp-gradient-top");
+var gb = document.querySelector(".ytp-gradient-bottom");
+var tt = document.querySelector(".ytp-title");
+gt.style.display = "none";
+gb.style.display = "none";
+tt.style.display = "none";
+console.log(gt);
+console.log(tt);
+
+
